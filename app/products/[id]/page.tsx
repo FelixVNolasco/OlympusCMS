405ffac69400501b092860937eae0da5a975c408
Felix Enrique Vega Nolasco <49852681+FelixVNolasco@users.noreply.github.com>
2023-01-30 21:34:30 -0600

    .

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,7 +1,7 @@
 
 import Image from 'next/image';
+import Link from 'next/link';
 import { getProduct } from '../../lib/api/products';
-;
 
 export default async function page({ params }: any) {
 
@@ -28,7 +28,7 @@ export default async function page({ params }: any) {
               <p className='text-lg font-semibold'>${price}</p>
             </div>
             <div className="flex justify-center items-center gap-4 transition ease-in-out duration-150">
-              <button className='text-stone-50 bg-gray-700 w-full p-2 rounded-md font-semibold cursor-pointer disabled:bg-gray-400 disabled:text-gray-600 disabled:cursor-not-allowed'>Actualizar</button>
+              <Link href={`/products/modify/${product._id}`} className='text-stone-50 bg-gray-700 w-full p-2 rounded-md font-semibold cursor-pointer disabled:bg-gray-400 disabled:text-gray-600 disabled:cursor-not-allowed'>Actualizar</Link>
             </div>
           </div>
         </div>
--- app/products/modify/[id]/page.tsx
+++ app/products/modify/[id]/page.tsx
@@ -1,96 +1,98 @@
-import { Formik, Form, Field, ErrorMessage } from "formik";
-import { getProduct } from "../../../lib/api/products";
+// "use client"
 
-interface UpdateProduct { img: string, title: string, categories: Array<String>, desc: string, price: number }
+// import { Formik, Form, Field, ErrorMessage } from "formik";
+// import { getProduct } from "../../../lib/api/products";
+
+// interface UpdateProduct { img: string, title: string, categories: Array<String>, desc: string, price: number }
 
 export default async function page({ params }: any) {
 
-  const { id } = params;
-  const product = await getProduct(id);
-  const { img, title, categories, desc, price, } = product;
+  // const { id } = params;
+  // const product = await getProduct(id);
+  // const { img, title, categories, desc, price, } = product;
 
   return (
-
-    <Formik
-      initialValues={{ img, title, categories, desc, price }}
-      validate={(values: any) => {
-        const errors: UpdateProduct = { img, title, categories, desc, price };
-        if (!values.img) {
-          errors.img = "Image is required";
-        }
-        if (!values.password) {
-          errors.title = "Title is required";
-        }
-        if (!values.desc) {
-          errors.title = "Description is required";
-        }
-        if (!values.price) {
-          errors.title = "Price is required";
-        }
-        return errors;
-      }}
-      onSubmit={async (values: UpdateProduct, { setSubmitting }: { setSubmitting: any }) => {
-        try {
-          setSubmitting(true);
-          const response: any = await fetch(
-            `https://olympus-backend.vercel.app/api/products/${id}`,
-            {
-              method: "POST",
-              // body: values
-            }
-          );
-          const userData = response.data;
-          // dispatch(loginSuccess(userData));
-          // navigate("/");
-          // setSubmitting(false);
-        } catch (error) {
-          setSubmitting(false);
-        }
-      }}
-    >
-      {({ isSubmitting }: { isSubmitting: any }) => (
-        <Form>
-          <label className="text-md" htmlFor="username">
-            Titel
-          </label>
-          <Field
-            className="rounded-md p-2 border-2 border-slate-400 transition ease-in-out duration-300 focus:outline-none focus:border-2 focus:border-slate-500 w-full"
-            type="username"
-            name="username"
-            value={title}
-          />
-          <ErrorMessage
-            className="text-red-500"
-            name="username"
-            component="div"
-          />
-          <div className="mt-4">
-            <label className="text-md" htmlFor="password">
-              Description
-            </label>
-            <Field
-              className="rounded-md p-2 border-2 border-slate-400 transition ease-in-out duration-300 focus:outline-none focus:border-2 focus:border-slate-500 w-full"
-              type="desc"
-              name="desc"
-              value={desc}
-            />
-            <ErrorMessage
-              className="text-red-500"
-              name="username"
-              component="div"
-            />
-          </div>
-          <div className="flex justify-end mt-4">
-            <button
-              className="p-3 bg-green-200 transition ease-in-out duration-300 hover:bg-green-300 rounded-md text-sm font-semibold cursor-pointer"
-              type="submit"
-              disabled={isSubmitting}
-            >
-              Actualizar
-            </button>
-          </div>
-        </Form>
-      )}
-    </Formik>
+    <p>Hello World</p>
+    // <Formik
+    //   initialValues={{ img, title, categories, desc, price }}
+    //   validate={(values: any) => {
+    //     const errors: UpdateProduct = { img, title, categories, desc, price };
+    //     if (!values.img) {
+    //       errors.img = "Image is required";
+    //     }
+    //     if (!values.password) {
+    //       errors.title = "Title is required";
+    //     }
+    //     if (!values.desc) {
+    //       errors.title = "Description is required";
+    //     }
+    //     if (!values.price) {
+    //       errors.title = "Price is required";
+    //     }
+    //     return errors;
+    //   }}
+    //   onSubmit={async (values: UpdateProduct, { setSubmitting }: { setSubmitting: any }) => {
+    //     try {
+    //       setSubmitting(true);
+    //       const response: any = await fetch(
+    //         `https://olympus-backend.vercel.app/api/products/${id}`,
+    //         {
+    //           method: "POST",
+    //           // body: values
+    //         }
+    //       );
+    //       const userData = response.data;
+    //       // dispatch(loginSuccess(userData));
+    //       // navigate("/");
+    //       // setSubmitting(false);
+    //     } catch (error) {
+    //       setSubmitting(false);
+    //     }
+    //   }}
+    // >
+    //   {({ isSubmitting }: { isSubmitting: any }) => (
+    //     <Form>
+    //       <label className="text-md" htmlFor="username">
+    //         Titel
+    //       </label>
+    //       <Field
+    //         className="rounded-md p-2 border-2 border-slate-400 transition ease-in-out duration-300 focus:outline-none focus:border-2 focus:border-slate-500 w-full"
+    //         type="username"
+    //         name="username"
+    //         // value={title}
+    //       />
+    //       <ErrorMessage
+    //         className="text-red-500"
+    //         name="username"
+    //         component="div"
+    //       />
+    //       <div className="mt-4">
+    //         <label className="text-md" htmlFor="password">
+    //           Description
+    //         </label>
+    //         <Field
+    //           className="rounded-md p-2 border-2 border-slate-400 transition ease-in-out duration-300 focus:outline-none focus:border-2 focus:border-slate-500 w-full"
+    //           type="desc"
+    //           name="desc"
+    //           // value={desc}
+    //         />
+    //         <ErrorMessage
+    //           className="text-red-500"
+    //           name="username"
+    //           component="div"
+    //         />
+    //       </div>
+    //       <div className="flex justify-end mt-4">
+    //         <button
+    //           className="p-3 bg-green-200 transition ease-in-out duration-300 hover:bg-green-300 rounded-md text-sm font-semibold cursor-pointer"
+    //           type="submit"
+    //           disabled={isSubmitting}
+    //         >
+    //           Actualizar
+    //         </button>
+    //       </div>
+    //     </Form>
+    //   )}
+    // </Formik>
   )
 }
