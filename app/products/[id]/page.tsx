b4e916360333a66f0d551668d1fe667965c71711
Felix Enrique Vega Nolasco <49852681+FelixVNolasco@users.noreply.github.com>
2023-01-30 21:27:29 -0600

    change layout in single product page

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,9 +1,7 @@
-import Image from 'next/image'
-import Link from 'next/link';
-import { getProduct } from '../../lib/api/products';
-import { Formik, Form, Field, ErrorMessage } from "formik";
 
-interface UpdateProduct { img: string, title: string, categories: Array<String>, desc: string, price: number }
+import Image from 'next/image';
+import { getProduct } from '../../lib/api/products';
+;
 
 export default async function page({ params }: any) {
 
@@ -12,87 +10,29 @@ export default async function page({ params }: any) {
   const { img, title, categories, desc, price, } = product;
 
   return (
-
-    <Formik
-      initialValues={{ img, title, categories, desc, price }}
-      validate={(values: any) => {
-        const errors: UpdateProduct = { img, title, categories, desc, price };
-        if (!values.img) {
-          errors.img = "Image is required";
-        }
-        if (!values.password) {
-          errors.title = "Title is required";
-        }
-        if (!values.desc) {
-          errors.title = "Description is required";
-        }
-        if (!values.price) {
-          errors.title = "Price is required";
-        }
-        return errors;
-      }}
-      onSubmit={async (values: UpdateProduct, { setSubmitting }: { setSubmitting: any }) => {
-        try {
-          setSubmitting(true);
-          const response: any = await fetch(
-            `https://olympus-backend.vercel.app/api/products/${id}`,
-            {
-              method: "POST",
-              // body: values
-            }
-          );
-          const userData = response.data;
-          // dispatch(loginSuccess(userData));
-          // navigate("/");
-          // setSubmitting(false);
-        } catch (error) {
-          setSubmitting(false);
-        }
-      }}
-    >
-      {({ isSubmitting }: { isSubmitting: any }) => (
-        <Form>
-          <label className="text-md" htmlFor="username">
-            Titel
-          </label>
-          <Field
-            className="rounded-md p-2 border-2 border-slate-400 transition ease-in-out duration-300 focus:outline-none focus:border-2 focus:border-slate-500 w-full"
-            type="username"
-            name="username"
-            value={title}
-          />
-          <ErrorMessage
-            className="text-red-500"
-            name="username"
-            component="div"
-          />
-          <div className="mt-4">
-            <label className="text-md" htmlFor="password">
-              Description
-            </label>
-            <Field
-              className="rounded-md p-2 border-2 border-slate-400 transition ease-in-out duration-300 focus:outline-none focus:border-2 focus:border-slate-500 w-full"
-              type="desc"
-              name="desc"
-              value={desc}
-            />
-            <ErrorMessage
-              className="text-red-500"
-              name="username"
-              component="div"
-            />
+    <div className='lex flex-col mt-6 m-auto sm:mt-6 sm:m-auto md:mt-6 md:m-auto lg:mt-6 lg:m-auto xl:mt-2 xl:mr-6 2xl:mt-2 2xl:mr-6 w-5/6  sm:border-slate-400 border-0 sm:border-2 rounded-lg drop-shadow-lg sm:shadow-sm shadow-slate-500 p-10'>
+      <main className="grid justify-items-center lg:mt-32 xl-mt-64 mb-64">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 lg:gap-12 w-10/12 2xl:w-9/12 items-center">
+          <div className='flex justify-center'>
+            <Image className='rounded-lg shadow-lg' src={img} width={520} height={520} alt="Product Image" />
           </div>
-          <div className="flex justify-end mt-4">
-            <button
-              className="p-3 bg-green-200 transition ease-in-out duration-300 hover:bg-green-300 rounded-md text-sm font-semibold cursor-pointer"
-              type="submit"
-              disabled={isSubmitting}
-            >
-              Actualizar
-            </button>
+          <div className="flex flex-col">
+            <h2 className='text-4xl text-center lg:text-left text-gray-800 font-semibold'>{title}</h2>
+            <div className='flex gap-4 mt-1 mb-4'>
+              {categories.map((category: string) => {
+                return <span key={category} className="px-1 rounded-md bg-gray-300">{category}</span>
+              })}
+            </div>
+            <p className='text-lg'>{desc}</p>
+            <div className="flex justify-end mb-4">
+              <p className='text-lg font-semibold'>${price}</p>
+            </div>
+            <div className="flex justify-center items-center gap-4 transition ease-in-out duration-150">
+              <button className='text-stone-50 bg-gray-700 w-full p-2 rounded-md font-semibold cursor-pointer disabled:bg-gray-400 disabled:text-gray-600 disabled:cursor-not-allowed'>Actualizar</button>
+            </div>
           </div>
-        </Form>
-      )}
-    </Formik>
+        </div>
+      </main>
+    </div>
   )
 }
