800c3b6a56d3ee05c24474241a864c32f0811898
Felix Enrique Vega Nolasco <49852681+FelixVNolasco@users.noreply.github.com>
2023-01-30 21:23:20 -0600

    first approach in the update product page

--- app/products/modify/[id]/page.tsx
+++ app/products/modify/[id]/page.tsx
@@ -1,7 +1,37 @@
-import React from 'react'
 
-export default function page() {
+import Image from 'next/image';
+import { getProduct } from '../../../lib/api/products';
+
+export default async function page({ params }: any) {
+
+  const { id } = params;
+  const product = await getProduct(id);
+  const { img, title, categories, desc, price, } = product;
+
   return (
-    <div>page</div>
+    <div className='lex flex-col mt-6 m-auto sm:mt-6 sm:m-auto md:mt-6 md:m-auto lg:mt-6 lg:m-auto xl:mt-2 xl:mr-6 2xl:mt-2 2xl:mr-6 w-5/6  sm:border-slate-400 border-0 sm:border-2 rounded-lg drop-shadow-lg sm:shadow-sm shadow-slate-500 p-10'>
+      <main className="grid justify-items-center lg:mt-32 xl-mt-64 mb-64">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 lg:gap-12 w-10/12 2xl:w-9/12 items-center">
+          <div className='flex justify-center'>
+            <Image className='rounded-lg shadow-lg' src={img} width={520} height={520} alt="Product Image" />
+          </div>
+          <div className="flex flex-col">
+            <h2 className='text-4xl text-center lg:text-left text-gray-800 font-semibold'>{title}</h2>
+            <div className='flex gap-4 mt-1 mb-4'>
+              {categories.map((category: string) => {
+                return <span key={category} className="px-1 rounded-md bg-gray-300">{category}</span>
+              })}
+            </div>
+            <p className='text-lg'>{desc}</p>
+            <div className="flex justify-end mb-4">
+              <p className='text-lg font-semibold'>${price}</p>
+            </div>
+            <div className="flex justify-center items-center gap-4 transition ease-in-out duration-150">
+              <button className='text-stone-50 bg-gray-700 w-full p-2 rounded-md font-semibold cursor-pointer disabled:bg-gray-400 disabled:text-gray-600 disabled:cursor-not-allowed'>Actualizar</button>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
   )
 }
