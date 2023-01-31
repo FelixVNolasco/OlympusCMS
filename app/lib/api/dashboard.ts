83647349ebb3f202c1fbd58a519fa704e4a253a1
Felix Enrique Vega Nolasco <49852681+FelixVNolasco@users.noreply.github.com>
2023-01-30 21:01:56 -0600

    updated admin token

--- app/lib/api/dashboard.ts
+++ app/lib/api/dashboard.ts
@@ -1,6 +1,6 @@
 
 export const getLatestTrasactions = () => {
-    return fetch("https://olympus-backend.vercel.app/api/orders/latest", { cache: "no-store", headers: { token: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYzBlMmVjODc3MmMwYTQ0YmIwMTQxNiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY3NDgzMzQ3OCwiZXhwIjoxNjc1MDkyNjc4fQ.NFqi2E972sM-lLxqdeC6nahr8SbxWGmLfloojy-viM0` } })
+    return fetch("https://olympus-backend.vercel.app/api/orders/latest", { cache: "no-store", headers: { token: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYzBlMmVjODc3MmMwYTQ0YmIwMTQxNiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY3NTEzNDAyNCwiZXhwIjoxNjc1MzkzMjI0fQ.iFMkx-qqqtbNMphwkXyhSZeno6is0bwziF9sgUk6IX8` } })
         .then((res) => res.json())
 }
 
--- app/lib/api/users.ts
+++ app/lib/api/users.ts
@@ -1,16 +1,16 @@
 
 export const getAllUsers = () => {
-    return fetch("https://olympus-backend.vercel.app/api/users/", { cache: "no-store", headers: { token: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYzBlMmVjODc3MmMwYTQ0YmIwMTQxNiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY3NDgzMzQ3OCwiZXhwIjoxNjc1MDkyNjc4fQ.NFqi2E972sM-lLxqdeC6nahr8SbxWGmLfloojy-viM0` } })
+    return fetch("https://olympus-backend.vercel.app/api/users/", { cache: "no-store", headers: { token: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYzBlMmVjODc3MmMwYTQ0YmIwMTQxNiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY3NTEzNDAyNCwiZXhwIjoxNjc1MzkzMjI0fQ.iFMkx-qqqtbNMphwkXyhSZeno6is0bwziF9sgUk6IX8` } })
         .then((res) => res.json())
 }
 
 export const getUser = (id: string) => {
-    return fetch(`https://olympus-backend.vercel.app/api/users/find/${id}`, { cache: "no-store", headers: { token: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYzBlMmVjODc3MmMwYTQ0YmIwMTQxNiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY3NDg0MTg4MCwiZXhwIjoxNjc1MTAxMDgwfQ.vH_o664tcq8DE82iewEuyWIS14dnDsyj20DmEO5NexY` } })
+    return fetch(`https://olympus-backend.vercel.app/api/users/find/${id}`, { cache: "no-store", headers: { token: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYzBlMmVjODc3MmMwYTQ0YmIwMTQxNiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY3NTEzNDAyNCwiZXhwIjoxNjc1MzkzMjI0fQ.iFMkx-qqqtbNMphwkXyhSZeno6is0bwziF9sgUk6IX8` } })
         .then((res) => res.json())
 }
 
 export const getLatestUser = () => {
-    return fetch("https://olympus-backend.vercel.app/api/users/?new=null", { cache: "no-store", headers: { token: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYzBlMmVjODc3MmMwYTQ0YmIwMTQxNiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY3NDgzMzQ3OCwiZXhwIjoxNjc1MDkyNjc4fQ.NFqi2E972sM-lLxqdeC6nahr8SbxWGmLfloojy-viM0` } })
+    return fetch("https://olympus-backend.vercel.app/api/users/?new=null", { cache: "no-store", headers: { token: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYzBlMmVjODc3MmMwYTQ0YmIwMTQxNiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY3NTEzNDAyNCwiZXhwIjoxNjc1MzkzMjI0fQ.iFMkx-qqqtbNMphwkXyhSZeno6is0bwziF9sgUk6IX8` } })
         .then((res) => res.json())
 }
 
